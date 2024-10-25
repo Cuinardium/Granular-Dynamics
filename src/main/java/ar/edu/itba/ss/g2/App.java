b91eea832de299f6889cee5f1366dd387d00511e
Santiago Ballerini <sballerini@itba.edu.ar>
2024-10-25 12:36:51 -0300

    feat: generate particles in main

--- src/main/java/ar/edu/itba/ss/g2/App.java
+++ src/main/java/ar/edu/itba/ss/g2/App.java
@@ -1,10 +1,44 @@
 package ar.edu.itba.ss.g2;
 
-/**
- * Hello world!
- */
+import ar.edu.itba.ss.g2.config.ArgParser;
+import ar.edu.itba.ss.g2.config.Config;
+import ar.edu.itba.ss.g2.generation.ObstacleGenerator;
+import ar.edu.itba.ss.g2.generation.ParticleGenerator;
+import ar.edu.itba.ss.g2.model.Particle;
+
+import java.util.List;
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ArgParser parser = new ArgParser(args);
+        Config config = parser.parse();
+
+        // TODO: seed
+        Random random = new Random();
+
+        ObstacleGenerator obstacleGenerator =
+                new ObstacleGenerator(
+                        config.getWidth(),
+                        config.getLength(),
+                        config.getObstacleCount(),
+                        config.getObstacleRadius(),
+                        random);
+
+        List<Particle> obstacles = obstacleGenerator.generate();
+
+        ParticleGenerator particleGenerator =
+                new ParticleGenerator(
+                        config.getWidth(),
+                        config.getLength(),
+                        config.getParticleCount(),
+                        config.getParticleRadius(),
+                        config.getParticleMass(),
+                        config.getAcceleration(),
+                        obstacles,
+                        random);
+
+        List<Particle> particles = particleGenerator.generate();
     }
 }
