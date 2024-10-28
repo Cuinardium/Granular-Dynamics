43cda8cd3543d379f0bc27865e1f6f3cb63d03d7
Santiago Ballerini <sballerini@itba.edu.ar>
2024-10-27 22:10:43 -0300

    feat: implement main method

--- src/main/java/ar/edu/itba/ss/g2/App.java
+++ src/main/java/ar/edu/itba/ss/g2/App.java
@@ -5,40 +5,77 @@ import ar.edu.itba.ss.g2.config.Config;
 import ar.edu.itba.ss.g2.generation.ObstacleGenerator;
 import ar.edu.itba.ss.g2.generation.ParticleGenerator;
 import ar.edu.itba.ss.g2.model.Particle;
+import ar.edu.itba.ss.g2.simulation.Simulation;
+import ar.edu.itba.ss.g2.util.FileUtil;
 
 import java.util.List;
 import java.util.Random;
 
 public class App {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        ArgParser parser = new ArgParser(args);
-        Config config = parser.parse();
+    ArgParser parser = new ArgParser(args);
+    Config config = parser.parse();
 
-        // TODO: seed
-        Random random = new Random();
-
-        ObstacleGenerator obstacleGenerator =
-                new ObstacleGenerator(
-                        config.getWidth(),
-                        config.getLength(),
-                        config.getObstacleCount(),
-                        config.getObstacleRadius(),
-                        random);
-
-        List<Particle> obstacles = obstacleGenerator.generate();
-
-        ParticleGenerator particleGenerator =
-                new ParticleGenerator(
-                        config.getWidth(),
-                        config.getLength(),
-                        config.getParticleCount(),
-                        config.getParticleRadius(),
-                        config.getParticleMass(),
-                        config.getAcceleration(),
-                        obstacles,
-                        random);
-
-        List<Particle> particles = particleGenerator.generate();
+    if (config == null) {
+      System.err.println("Error while parsing arguments");
+      System.exit(1);
     }
+
+    // TODO: seed
+    Random random = new Random();
+
+    ObstacleGenerator obstacleGenerator =
+        new ObstacleGenerator(
+            config.getWidth(),
+            config.getLength(),
+            config.getObstacleCount(),
+            config.getObstacleRadius(),
+            random);
+
+    List<Particle> obstacles = obstacleGenerator.generate();
+
+    ParticleGenerator particleGenerator =
+        new ParticleGenerator(
+            config.getWidth(),
+            config.getLength(),
+            config.getParticleCount(),
+            config.getParticleRadius(),
+            config.getParticleMass(),
+            config.getAcceleration(),
+            obstacles,
+            random);
+
+    List<Particle> particles = particleGenerator.generate();
+
+    Simulation simulation = new Simulation(
+        particles,
+        obstacles,
+        config.getWidth(),
+        config.getLength(),
+        config.getAcceleration(),
+        config.getNormalK(),
+        config.getTangentialK(),
+        config.getIntegrationStep(),
+        config.getSnapshotStep(),
+        config.getMaxTime());
+
+
+    simulation.run();
+
+    List<List<Particle>> snapshots = simulation.getSnapshots();
+    List<Double> dischargeTimes = simulation.getDischargeTimes();
+
+    try {
+      FileUtil.serializeConfig(config, config.getOutputDirectory());
+      FileUtil.serializeObstacles(obstacles, config.getOutputDirectory());
+      FileUtil.serializeDischarges(dischargeTimes, config.getOutputDirectory());
+      FileUtil.serializeSnapshots(snapshots, config.getSnapshotStep(), config.getOutputDirectory());
+    } catch (Exception e) {
+      System.err.println("Error while serializing output");
+      System.exit(1);
+    }
+
+    System.exit(0);
+  }
 }
