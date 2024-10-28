39855c1052f1a6f4f8375a0cd3a83d9a32973a3b
Gonzalo A. Martone <gmartone@itba.edu.ar>
2024-10-28 17:34:15 -0300

    feat: implement getNeighbours

--- src/main/java/ar/edu/itba/ss/g2/simulation/CellIndexMethod.java
+++ src/main/java/ar/edu/itba/ss/g2/simulation/CellIndexMethod.java
@@ -8,12 +8,77 @@ public class CellIndexMethod {
 
     private final int M;
     private final int L;
-    private final double rc;
 
-    public CellIndexMethod(int M, int L, double rc) {
+    public CellIndexMethod(int M, int L) {
         this.M = M;
         this.L = L;
-        this.rc = rc;
+    }
+
+    public Map<Particle, Set<Particle>> getNeighbours(Set<Particle> particles) {
+        Map<Particle, Set<Particle>> map = new HashMap<>();
+
+        List<List<Set<Particle>>> grid = generateGrid(particles);
+
+        for(int x = 0; x < M; x++) {
+            for(int y = 0; y < M; y++) {
+                for(Particle p : grid.get(x).get(y)) {
+                    checkAdjacent(x, y, grid, p, map);
+                }
+            }
+        }
+
+        return map;
+    }
+
+    private void addIfClose(Particle p1, Particle p2, Map<Particle, Set<Particle>> map) {
+        if (p1.overlaps(p2)) {
+            if (!map.containsKey(p1)) {
+                map.put(p1, new HashSet<>());
+            }
+            if (!map.containsKey(p2)) {
+                map.put(p2, new HashSet<>());
+            }
+            map.get(p1).add(p2);
+            map.get(p2).add(p1);
+        }
+    }
+
+    private void checkAdjacent(int x, int y, List<List<Set<Particle>>> grid, Particle p, Map<Particle, Set<Particle>> map) {
+        if (x < 0 || y < 0 || x >= grid.size() || y >= grid.get(x).size()) {
+            return;
+        }
+
+        // tengo
+        // - - -
+        // - x -
+        // - - -
+
+        // reviso
+        // - x x
+        // - x x
+        // - - -
+        for (int i = Math.max(x - 1, 0); i <= x; i++) {
+            for (int j = y; j <= Math.min(y + 1, grid.size() - 1); j++) {
+                for (Particle p2 : grid.get(i).get(j)) {
+                    if (!p.equals(p2)) {
+                        addIfClose(p, p2, map);
+                    }
+                }
+            }
+        }
+        // reviso
+        // - - -
+        // - - -
+        // - - x
+        x = x + 1;
+        y = y + 1;
+        if (!(x >= grid.size() || y >= grid.get(x).size())) {
+            for (Particle p2 : grid.get(x).get(y)) {
+                if (!p.equals(p2)) {
+                    addIfClose(p, p2, map);
+                }
+            }
+        }
     }
 
     private List<List<Set<Particle>>> generateGrid(Set<Particle> particles) {
